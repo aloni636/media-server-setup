1389f0cb4aa1b73c7f2fe486e3d5b198be4f32d2
aloni636 <elyasaf.aloni@gmail.com>
2025-08-22 16:35:10 +0300

    feat(storage): made media storage directory configurable

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 # Security
 - `CHOWN` capability is necessary for root directories permission modification as startup (like `/app` for Jellyfin, etc).
 
-## Mounting Hard Drives
+# Mounting Hard Drives
 - On Rapberry Pi OS disable automount with `sudo systemctl disable udisks2.service` to have explicit contorl over mounting.
 - Run `lsblk -f` to gather details about the hard drive.
 - Fill the details (devide UUID and fstype) in a new `/etc/fstab` entry. Fill the rest of the options for your liking: 
@@ -18,3 +18,9 @@
 - Reload systemd cached fstab entries: `sudo systemctl daemon-reload`
 - Mount (first time) using `sudo mount -a` or (after modifying fstab) `sudo mount -o remount <mountpoint>`
 - For filesystems which store ownership and permissions, setup permissions for the mount point by using: `sudo chown -R <user id>:<user group> <mountpoint>`
+
+# Config
+Use `.env.example` for the list of **cross compose services** configuration. Create a `.env`
+
+# Setup Hints
+- **Qbittorrent** uses `admin` username and generates a one time admin password for first time login.
--- setup.sh
+++ setup.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
-set -e
+set -e  # early exit when facing errors
 
-BASE_DIR="${HOME}/media-server/data"
+# Verify .env file existence
+if [ -f ./.env ]
+then
+  export $(cat .env | xargs)
+else
+  echo "Cannot find .env file. Make sure it exists. Use ./.env.example as a reference."
+  exit 1
+fi
+
+# Verify required environment variables
+for var in DATA; do
+  if [ -z "${!var}" ]; then
+    echo "Missing $var environment variable. Check ./.env file."
+    exit 1
+  else
+    echo "Loaded environment variable: $var=${!var}"
+  fi
+done
 
 echo "Creating directories..."
-mkdir -p "${BASE_DIR}"/torrents
-mkdir -p "${BASE_DIR}"/media/{tv,movies}
+mkdir -p "${DATA}"/torrents
+mkdir -p "${DATA}"/media/{tv,movies}
 
 echo "Fixing ownership and permissions..."
-chown --recursive 1000:1000 "${BASE_DIR}"
+sudo chown --recursive 1000:1000 "${DATA}"
 chmod --recursive +x *-custom-cont-init.d
 
 read -p "Do you want to start Docker Compose? [y/N]" answer
 if [[ "$answer" == "y" ]]; then 
-    echo "Starting Docker Compose..."
-    cd "$(dirname "$0")"
-    docker compose -f compose.yaml up -d
+  echo "Starting Docker Compose..."
+  cd "$(dirname "$0")"
+  docker compose -f compose.yaml up -d
 fi
 
